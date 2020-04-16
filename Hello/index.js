eb8e84dc3ca5602df6bc5859905bcafd76aa0206
Anthony Chu <antchu@microsoft.com>
2020-04-16 13:26:15 -0700

    Initial commit

---
+++ Hello/index.js
@@ -0,0 +1,18 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+/**
+ * @type {import('@azure/functions').AzureFunction}
+ * @param {import('@azure/functions').Context} context
+ */
+module.exports = async function (context) {
+    return `Hello ${context.bindings.name}!`;
+};
---
+++ HttpStart/index.js
@@ -0,0 +1,15 @@
+﻿const df = require("durable-functions");
+
+/**
+ * @type {import('@azure/functions').AzureFunction}
+ * @param {import('@azure/functions').Context} context
+ * @param {import('@azure/functions').HttpRequest} req
+ */
+module.exports = async function (context, req) {
+    const client = df.getClient(context);
+    const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
+
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+};
